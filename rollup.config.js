05b77759b539ffaf2f53316dac061f7e4ceea0da
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-07 20:24:02 +0300

    Init repo

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+
+function onwarn(warning) {
+  if (warning.code === 'THIS_IS_UNDEFINED')
+    return;
+  console.error(warning.message);
+}
+
+const outFolder = 'dist';
+
+export default [
+  {
+    input: 'src/main.js',
+    output: {
+      file: `${outFolder}/main.min.js`,
+      format: 'iife',
+      name: 'WebElements'
+    },
+    onwarn,
+    plugins: [resolve(), terser({
+      output: {
+        comments: `/xxyyzzz/`
+      }
+    })]
+  },
+  {
+    input: 'src/showcase.js',
+    output: {
+      file: `${outFolder}/showcase.min.js`,
+      format: 'iife',
+      name: 'WebElementsShowcase'
+    },
+    onwarn,
+    plugins: [resolve(), terser({
+      output: {
+        comments: `/xxyyzzz/`
+      }
+    })]
+  }
+];
